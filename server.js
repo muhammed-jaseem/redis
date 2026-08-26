4d74a24df82f35daefa89da89a167bb50bfaeb4c
Muhammed Jaseem <muhammed.jaseem@trenser.com>
2026-08-26 18:59:50 +0530

    Add basic operations

--- server.js
+++ server.js
@@ -15,6 +15,31 @@ async function testRedisConnection() {
   try {
     await client.connect();
     console.log("Connected to redis");
+
+    await client.set("name", "Samuel");
+    const extractedValue = await client.get("name");
+    console.log(extractedValue);
+
+    const deleteCount = await client.del("name");
+    console.log(deleteCount);
+
+    const updatedValue = await client.get("name");
+    console.log(updatedValue);
+
+    const countValue = await client.set("count", 52);
+    const extractedCountValue = await client.get("count");
+    console.log(extractedCountValue);
+
+    const incrementedValue = await client.incr("count");
+    console.log(incrementedValue);
+
+    await client.decr("count");
+    await client.decr("count");
+    await client.decr("count");
+    await client.decr("count");
+    await client.decr("count");
+
+    console.log(await client.get("count"));
   } catch (error) {
     console.log(error);
   } finally {
